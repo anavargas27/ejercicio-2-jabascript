c562337b0b2a861bd49cbae1a0b14888bb88e31a
anavargas27 <112999745+anavargas27@users.noreply.github.com>
2022-12-16 21:30:42 -0300

    Add files via upload

---
+++ ejercicio-02.js
@@ -0,0 +1,10 @@
+const lista1 = ["Ana", 27, verdadero,
+    new Date(1995, 02, 27),
+    {
+        titulo: "the Secret Lives of introverts",
+        autor: "Jenn Granneman",
+        fecha: new Date(2017, 08, 1),
+        url: "https://www.amazon.com/Secret-Lives-Introverts-Inside-Hidden/dp/1510721029",
+    },
+
+];
